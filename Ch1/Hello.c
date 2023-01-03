7542f0add8781bb054c90441d622492832f6f57a
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-01-04 00:31:20 +0900

    1_3

---
+++ Ch1/Hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World! \n\n");
+	printf("È«±æµ¿ \nÈ« ±æ µ¿ \nÈ«  ±æ  µ¿ \n\n");   
+	printf("±è¹Ì¸£ \nxx½Ã xx±¸ \n010-xxxx-xxxx \n");   // abcd 
+													   // efgh		
+	return 0;
+}
---
+++ Ch1/ch2-3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main_2(void)
+{
+	printf("%d\n", 1234);
+	printf("%d %d\n", 10, 20);
+	return 0;
+}
