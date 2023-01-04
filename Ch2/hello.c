835766ec35c3049013d72884f5f017f9e833bd5e
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-01-04 16:53:18 +0900

    Update hello.c

--- Ch2/hello.c
+++ Ch2/hello.c
@@ -2,9 +2,11 @@
 
 int main(void)
 {
-	printf("Hello world \n\n");		//林籍
-	printf("全辨悼\n全 辨 悼\n全  辨  悼\n\n"); /* 林籍 */
+	printf("Hello world \n\n");		/* 林籍 */
+	printf("全辨悼\n全 辨 悼\n全  辨  悼\n\n"); // 巩力 2-1
+
 	printf("%d %d\n", 10, 20);   /*林
 									 籍*/
+	printf("%d X %d = %d \n%d X %d = %d", 4, 5, 20, 7, 9, 7*9);  // 巩力 2-2
 	return 0;
 }
