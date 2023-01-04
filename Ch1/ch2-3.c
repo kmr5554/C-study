7045e489c0d1efc281099b17fd2fbcb24539e6c7
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-01-04 16:30:14 +0900

    ch.2 practice

--- Ch1/ch2-3.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main_2(void)
-{
-	printf("%d\n", 1234);
-	printf("%d %d\n", 10, 20);
-	return 0;
-}
---
+++ Ch2/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello world \n\n");		//林籍
+	printf("全辨悼\n全 辨 悼\n全  辨  悼\n\n"); /* 林籍 */
+	printf("%d %d\n", 10, 20);   /*林
+									 籍*/
+	return 0;
+}
