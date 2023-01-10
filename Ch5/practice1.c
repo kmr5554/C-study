d5ff288af4d6a9830e9ec16ee1de67d417d49a2c
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-01-10 22:18:10 +0900

    ch5_2

--- Ch5/practice1.c
+++ Ch5/practice1.c
@@ -6,7 +6,7 @@ int main(void)
 	int inum = 1052;
 	double num = 3.1415;  // 실수 표현
 
-	printf("ch 크기 : %d\n", sizeof(ch));   // 크기 : 저장 바이트
+	printf("ch 크기 : %d\n", sizeof(ch));   // size of : 저장 바이트 크기 출력
 	printf("inum 크기 : %d\n", sizeof(inum));
 	printf("num 크기 : %d\n\n", sizeof(num));
 
@@ -15,5 +15,8 @@ int main(void)
 
 	printf("num1,num2 크기: %d, %d\n", sizeof(num1), sizeof(num2));
 	printf("ch added 크기 : %d\n", sizeof(num1 + num2));	// 연산은 가장 빠른 자료형으로 변환하므로 int 크기로 출력됨
-	printf("result1 크기  : %d\n", sizeof(result1));		// 변수 지정 형식인 char의 크기로 출력됨
+	printf("result1 크기  : %d\n\n", sizeof(result1));		// 변수 지정 형식인 char의 크기로 출력됨
+	
+	unsigned char num3 = 3;
+	printf("num3 크기 : %d\n", sizeof(num3));		// unsigned : 0 또는 양수만 가능, 대신 가능범위는 2배
 }
