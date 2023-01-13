3ab4ab69df350bb2fb25213442eb8a3fa9ef30fb
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-01-13 21:49:08 +0900

    update

--- ch6/practice1.c
+++ ch6/practice1.c
@@ -2,5 +2,13 @@
 
 int main(void)
 {
+	int num1 = 7, num2 = 13;
+	printf("%o, %#o \n", num1, num1);  // %o는 8진수 정수 표현 -> 7  /  8진수임을 보여주고 싶으면 %#o 사용 -> 07
+	printf("%x, %#x \n\n", num2, num2);  // %x는 16진수 정수 표현 -> d / 16진수임을 보여주고 싶으면 %#x 사용 -> 0xd
+
+	double d1 = 1.23e-3;
+	double d2 = 1.23e-6;
+	printf("%g %g \n", d1, d2);		// %g는 소수점 작으면 실수 형태, 소수점 자리 많으면 e 형태로 출력
+	return 0;
 
 }
