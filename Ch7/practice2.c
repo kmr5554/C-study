490a4fe2662f37754538b323399defbb1dd2a3d2
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-01-15 20:40:17 +0900

    update

--- Ch7/practice2.c
+++ Ch7/practice2.c
@@ -2,5 +2,14 @@
 
 int main(void)
 {
+	int total = 0, num = 0;
 
+	do    // 반복문 무조건 1회 이상 실행 시에는 do ~ while 을 사용
+	{
+		printf("정수(0 to quit) : ");
+		scanf("%d", &num);
+		total += num;
+	} while (num != 0);
+	printf("result : %d \n\n", total);
+	return 0;
 }
