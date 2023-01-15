a3b57327671665ca703ef43f90e286d5a89c9696
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-01-15 20:57:10 +0900

    update

--- Ch7/practice2.c
+++ Ch7/practice2.c
@@ -11,5 +11,24 @@ int main(void)
 		total += num;
 	} while (num != 0);
 	printf("result : %d \n\n", total);
+
+
+	for (int num1 = 0; num1 < 3; num1++)  //for 문 쓰면 변수 지정, 조건문, 증감식 하나로 묶을 수 있음
+		printf("Hi\n");
+	printf("\n");
+
+
+	double total1 = 0.0, input = 0.0;
+	int num2 = 0;
+
+	for (; input >= 0.0; )
+	{
+		total1 += input;
+		printf("input (minus to quit) : ");
+		scanf("%lf", &input);
+		num2++;
+	}
+	printf("avg : %f", total1 / (num2 - 1));
+
 	return 0;
 }
