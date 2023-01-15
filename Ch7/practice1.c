65cc1399d44fc7bfb8daab794a013ebea408bbab
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-01-15 17:36:28 +0900

    update

--- Ch7/practice1.c
+++ Ch7/practice1.c
@@ -7,6 +7,16 @@ int main(void)
 	while (num < 5)
 	{
 		printf("Hello world! %d\n", num);
-		num++;
+		num++;  // printf랑 같은 줄에 있어도 됨
+	}
+
+	int dan, num1 = 1; //dan은 자료형만 지정, num1은 숫자까지 저장
+	printf("몇 단? : ");
+	scanf("%d", &dan);
+
+	while (num1 < 10)
+	{
+		printf("%d X %d = %d\n", dan, num1, dan * num1);
+		num1+=1;
 	}
 }
