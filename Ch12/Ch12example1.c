78228fc113291d59d22caf081a424b377f153e9c
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-01-28 17:25:21 +0900

    update

--- Ch12/Ch12example1.c
+++ Ch12/Ch12example1.c
@@ -1,2 +1,34 @@
 #include <stdio.h>
 
+void problem1(void)
+{
+	int num = 10;
+	int* ptr1 = &num;
+	int* ptr2 = ptr1; // ptr2는 ptr1과 같음 --> num 저장된 주소 공유
+	(*ptr1)++, (*ptr2)++; // 둘 다 num을 증가시킴
+	printf("%d\n", num);
+}
+
+void problem2(void)
+{
+	int num3 = 10, num4 = 20;
+	int* ptr1 = &num3, * ptr2 = &num4;
+	(*ptr1)+=10, (*ptr2)-=10;
+	ptr1 = &num4;	
+	ptr2 = &num3;	
+
+	// *ptr1=&num4 할 경우 에러 발생
+	// 이유 : *ptr1는 num3을 의미하므로 num3 = &num4 꼴이 되기 때문
+
+	printf("ptr1 : %d, ptr2 :  %d", *ptr1, *ptr2);
+}
+
+int main(void)
+{
+	// 1번 문제
+	problem1();
+
+	// 2번 문제
+	problem2();
+
+}
---
+++ Ch13/practice1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void ex1(void)
+{
+
+}
+
+void ex2(void)
+{
+
+}
+
+int main(void)
+{
+
+}
