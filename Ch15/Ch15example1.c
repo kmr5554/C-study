b54583d73ea9f815a0a0878c4ccc5bd63bf77683
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-01-29 18:24:10 +0900

    update

---
+++ Ch15/Ch15example1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+void Odd(int *param, int len)
+{
+	printf("홀수 출력 : ");
+	for (int i = 0; i < len; i++)
+		if (param[i] % 2 == 1)
+			printf("%d ", param[i]);
+}
+void Even(int* param, int len)
+{
+	printf("\n짝수 출력 : ");
+	for (int i = 0; i < len; i++)
+		if (param[i] % 2 == 0)
+			printf("%d ", param[i]);
+}
+
+
+void Prob1(void)
+{
+	int n1, n2, n3, n4, n5, n6, n7, n8;
+	printf("Input : ");
+	scanf("%d %d %d %d %d %d %d %d", &n1, &n2, &n3, &n4, &n5, &n6, &n7, &n8);
+	int arr1[8] = { n1,n2,n3,n4,n5,n6,n7,n8 };
+
+	Odd(arr1, sizeof(arr1) / sizeof(int));
+	Even(arr1, sizeof(arr1) / sizeof(int));
+	printf("\n\n");
+}
+
+void Dec_to_Bi(int n)
+{
+	
+}
+
+void Prob2(void)
+{
+	int num2;
+	printf("10진수 num : "), scanf("%d", &num2);
+	Dec_to_Bi(num2);
+}
+
+
+int main(void)
+{
+	Prob1();
+}
