88bfd86340b9c9433115f7ae4195c4004748ec01
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-01-31 22:06:13 +0900

    update

---
+++ Ch15/Ch15Prob4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void Palindrome(char param[], int len)
+{
+	int i;
+	int a=1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		if (param[i] != param[len - 1 - i])
+		{
+			a = 0;
+			break;
+		}
+	}
+	if (a == 1)
+		printf("회문 입니다.\n");
+	if (a != 1)
+		printf("회문이 아닙니다.\n");
+}
+
+int main(void)
+{
+	char string[20];
+	int count=0,len;
+	printf("string : "), scanf("%s", string);
+	
+	while (string[count] != 0)
+		count++;
+	len = count;
+
+	Palindrome(string, len);
+	return 0;
+}
