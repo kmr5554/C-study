c994900ad830ee767e8ee76cf0efa3db1a32432e
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-02-12 00:17:32 +0900

    update

--- Ch17/Ch16Prob1.c
+++ Ch17/Ch16Prob1.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+void MaxAndMin(int *ary, int len, int **max, int **min)
+{
+	int i;
+	*max = &ary[0];
+	*min = &ary[0];
+
+	for (i = 0; i < len; i++)
+	{
+		if (ary[i] > **max)
+			*max = &ary[i];
+		if (ary[i] < **min)
+			*min = &ary[i];
+	}
+}
+
+
 int main(void)
 {
+	int* maxPtr;
+	int* minPtr;
+	int arr[5];
+
+	printf("arr[5] : ");
+	for (int i = 0; i < 5; i++)
+		scanf("%d", &arr[i]);
+
+
+	MaxAndMin(arr, sizeof(arr)/sizeof(int),&maxPtr,&minPtr);
+	printf("maxPtr, minPtr, max, min : %d, %d, %d, %d", maxPtr, minPtr, *maxPtr, *minPtr);
 
 }
