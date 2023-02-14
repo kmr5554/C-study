59354c81513d8ef59137625113a8e7f8b06c8eb7
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-02-14 18:18:05 +0900

    Update Ch21Problem1.c

--- Ch21/Ch21Problem1.c
+++ Ch21/Ch21Problem1.c
@@ -19,7 +19,7 @@ int main(void)
 
 	if (c == -1)
 	{
-		puts("Error! Enter Alphabet.");
+		puts("Error! Enter Alphabet.");		// puts 는 문자열을 출력함,   fputs는 스트림 지정해서 출력하며 \n을 실행하지 않음.
 		return -1;
 	}
 
