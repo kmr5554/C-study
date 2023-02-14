1cbb302183ebfa07baa574e53c85cd35518895c8
kmr5554 <103166653+kmr5554@users.noreply.github.com>
2023-02-14 18:16:35 +0900

    Update Ch21Problem1.c

--- Ch21/Ch21Problem1.c
+++ Ch21/Ch21Problem1.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
-void Convert(char ch)
+int Convert(int ch)
 {
 	int diff = 'a' - 'A';
+	if ('a' <= ch && ch <= 'z')
+		return ch-diff;
+	else if ('A' <= ch && ch <= 'Z')
+		return ch+diff;
+	else
+		return -1;
+}
 
+int main(void)
+{
+	int c;
+	c = getchar();
+	c=Convert(c);
+
+	if (c == -1)
+	{
+		puts("Error! Enter Alphabet.");
+		return -1;
+	}
+
+	putchar(c);
+	return 0;
+	
 }
